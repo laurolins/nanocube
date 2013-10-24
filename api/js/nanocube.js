4c5ab00a051f69e9eb127c22b3581216c9fd4847
Carlos Scheidegger <cscheid@research.att.com>
2013-10-24 16:17:08 -0400

    binary_xhr

--- api/js/nanocube.js
+++ api/js/nanocube.js
@@ -1,5 +1,33 @@
 var Nanocube = {};
 
+function binary_xhr(url, handler)
+{
+    var xhr = new window.XMLHttpRequest();
+    var ready = false;
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200
+            && ready !== true) {
+            if (xhr.responseType === "arraybuffer") {
+                handler(xhr.response, url);
+            } else if (xhr.mozResponseArrayBuffer !== null) {
+                handler(xhr.mozResponseArrayBuffer, url);
+            } else if (xhr.responseText !== null) {
+                var data = String(xhr.responseText);
+                var ary = new Array(data.length);
+                for (var i = 0; i <data.length; i++) {
+                    ary[i] = data.charCodeAt(i) & 0xff;
+                }
+                var uint8ay = new Uint8Array(ary);
+                handler(uint8ay.buffer, url);
+            }
+            ready = true;
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.responseType="arraybuffer";
+    xhr.send();
+};
+
 Nanocube.diff_queries = function(q1, q2)
 {
     var result = {
