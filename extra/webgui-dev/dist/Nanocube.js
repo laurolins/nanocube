e53f58de755fdf54143bf833f9b4f27d64023799
andrew7jin <andrew7jin@research.att.com>
2017-06-05 17:05:50 -0400

    Added the data import, working version!

--- extra/webgui-dev/dist/Nanocube.js
+++ extra/webgui-dev/dist/Nanocube.js
@@ -2105,12 +2105,12 @@ function Timeseries(opts,getDataCallback,updateCallback){
         }
     });
 
-    // opts.numformat = opts.numformat || ",";
+    opts.numformat = opts.numformat || ",";
 
-    // this._datasrc = opts.datasrc;
+    this._datasrc = opts.datasrc;
 
-    // widget.getDataCallback = getDataCallback;
-    // widget.updateCallback =  updateCallback;
+    widget.getDataCallback = getDataCallback;
+    widget.updateCallback =  updateCallback;
 
 
     var margin = opts.margin;
@@ -2120,15 +2120,17 @@ function Timeseries(opts,getDataCallback,updateCallback){
     var width = $(id).width() - margin.left - margin.right;
     var height = $(id).height() - margin.top - margin.bottom;
 
-    widget.x = d3.scaleUtc().range([0, width])
-    	.domain([new Date(2013, 7, 1), new Date(2013, 7, 15) - 1]); //temporary
-    widget.y = d3.scaleLinear().range([0,height]);
+    widget.x = d3.scaleUtc().range([0, width]);
+    widget.y = d3.scaleLinear().range([height, 0]);
 
     widget.xAxis = d3.axisBottom(widget.x)
     	.tickSize(-height);
     	
 
-    widget.yAxis = d3.axisLeft(widget.y);
+    widget.yAxis = d3.axisLeft(widget.y)
+    	.ticks(3)
+        .tickFormat(d3.format(opts.numformat))
+        .tickSize(-width-3);
 
     widget.x_new = widget.x;
     widget.zoom=d3.zoom()
@@ -2140,19 +2142,23 @@ function Timeseries(opts,getDataCallback,updateCallback){
         	if(widget.brushtime !== undefined){
         		widget.brush.move(widget.gbrush, widget.brushtime.map(widget.x_new));
         	}
-            // widget.redraw(widget.lastres);
+            widget.redraw(widget.lastres);
         })
         .on('end', function(){
         	//update data
-        	// widget.update();
-            // widget.updateCallback(widget._encodeArgs());
+        	widget.update();
+            widget.updateCallback(widget._encodeArgs());
         });
 
     widget.brush = d3.brushX()
 		.extent([[0, 0], [width, height]])
 		.on("end", function(){
 			if(!(d3.event.sourceEvent instanceof MouseEvent)) return;
-			if(!d3.event.selection) return;
+			if(!d3.event.selection){
+				widget.brushtime = undefined;
+				widget.updateCallback(widget._encodeArgs());
+				return;
+			}
 			var d0 = d3.event.selection.map(widget.x_new.invert),
 				d1 = d0.map(d3.utcDay.round);
 
@@ -2163,7 +2169,7 @@ function Timeseries(opts,getDataCallback,updateCallback){
 			}
 			widget.brushtime = d1;
 			d3.event.target.move(widget.gbrush, d1.map(widget.x_new));
-			// widget.updateCallback(widget._encodeArgs());
+			widget.updateCallback(widget._encodeArgs());
 		});
 
 	//Brush play button
@@ -2179,6 +2185,7 @@ function Timeseries(opts,getDataCallback,updateCallback){
             }
         }).html("Play");
 
+
     widget.svg = d3.select(id).append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -2188,6 +2195,17 @@ function Timeseries(opts,getDataCallback,updateCallback){
         .call(widget.zoom)
         .on("mousedown", function() { d3.event.stopPropagation(); });
 
+    if(opts.args){
+    	widget._decodeArgs(opts.args);
+    }
+    else{
+    	widget.x.domain(opts.timerange);
+    }
+
+    // widget.svg.append("text")
+    //     .attr("x", -10)
+    //     .attr("y", -10)
+    //     .text(opts.title);
 
     var gX = widget.svg.append("g")
         .attr("class", "axis axis--x")
@@ -2201,24 +2219,175 @@ function Timeseries(opts,getDataCallback,updateCallback){
     widget.gbrush = widget.svg.append("g")
     	.attr("class", "brush")
     	.call(widget.brush);
+
+    widget.width = width;
+    widget.gX = gX;
+    widget.gY = gY;
+
 }
 
 Timeseries.prototype={
 	update: function(){
-		return;
-	},
+        var widget = this;
+        var sel = this.getSelection();
+        var start = sel.global.start;
+        var end = sel.global.end;
+        var interval = (end - start+1) / 1000 / this.width * 1;
 
-	getSelection: function(){
+        var promises = {};
+
+        //generate promise for each expr
+        for (var d in widget._datasrc){
+            if (widget._datasrc[d].disabled){
+                continue;
+            }
+            var p = this.getDataCallback(d,start, end, interval);
+            for (var k in p){
+                promises[k] = p[k];
+            }
+        }
+            
+        var promarray = Object.keys(promises).map(function(k){
+            return promises[k];
+        });
+
+        var promkeys = Object.keys(promises);
+        $.when.apply($,promarray).done(function(){
+            var results = arguments;
+            var res = {};
+            promkeys.forEach(function(d,i){
+                res[d] = results[i];
+
+                var label = d.split('&-&');
+                var isColor  = /^#[0-9A-F]{6}$/i.test(label[0]);                
+                if(isColor){
+                    res[d].color = label[0];
+                }
+                else{
+                    var colormap = widget._datasrc[label[1]].colormap;
+                    var cidx = Math.floor(colormap.length/2);
+                    res[d].color = colormap[cidx];
+                }
+            });
+
+            widget.lastres = res;
+            widget.redraw(res);
+        });
+
+    },
+
+    getSelection: function(){
         var sel = {};
-        var timedom = this.x.domain();
+        var timedom = this.x_new.domain();
         sel.global = {start:timedom[0], end:timedom[1]};
 
-        // if (!this.brush.empty()){
-        //     var bext = this.brush.extent();
-        //     sel.brush = {start:bext[0], end:bext[1]};
-        // }
+        brushnode = this.gbrush.node();
+        if (brushnode !== null && d3.brushSelection(brushnode) !== null){
+            var bext = d3.brushSelection(brushnode).map(this.x_new.invert);
+            sel.brush = {start:bext[0], end:bext[1]};
+        }
         return sel;
     },
+
+    _encodeArgs: function(){
+        var args= this.getSelection();
+        return JSON.stringify(args);
+    },
+    
+    _decodeArgs: function(s){
+        var args = JSON.parse(s);
+        this.x.domain([new Date(args.global.start),
+                       new Date(args.global.end)]);
+        if(args.brush){
+            this.brush.move(this.brushg, 
+                            [this.x(new Date(args.brush.start)),
+                             this.x(new Date(args.brush.end))]);
+        }
+    },
+
+    redraw: function(lines){            
+        Object.keys(lines).forEach(function(k){
+            if(lines[k].data.length > 1){ 
+                var last = lines[k].data[lines[k].data.length-1];
+                lines[k].data.push(last); //dup the last point for step line
+            }
+        });
+
+        //update y axis
+        var yext = Object.keys(lines).reduce(function(p,c){
+            var e = d3.extent(lines[c].data, function(d){
+                return (d.val || 0);
+            });
+            return [ Math.min(p[0],e[0]),
+                     Math.max(p[1],e[1])];
+        }, [Infinity,-Infinity]);
+
+
+        yext[0]= yext[0]-0.05*(yext[1]-yext[0]); //show the line around min
+        yext[0]= Math.min(yext[0],yext[1]*0.5);
+        
+        this.y.domain(yext);
+
+        //update the axis
+        this.gX.call(this.xAxis);
+        this.gY.call(this.yAxis);
+
+        var widget = this;
+
+        //Remove paths obsolete paths
+        var paths = widget.svg.selectAll('path.line');
+        paths.each(function(){
+            var p = this;
+            var exists = Object.keys(lines).some(function(d){
+                return d3.select(p).classed(d);
+            });
+            if (!exists){ // remove obsolete
+                d3.select(p).remove();
+            }
+        });
+        
+        //Draw Lines
+        Object.keys(lines).forEach(function(k){
+            lines[k].data.sort(function(a,b){return a.time - b.time;});
+            widget.drawLine(lines[k].data,lines[k].color);
+        });
+    },
+
+    drawLine:function(data,color){
+        var colorid = 'color_'+color.replace('#','');
+        
+        if (data.length < 2){
+            return;
+        }
+
+        var widget = this;
+        
+        //create unexisted paths
+        var path = widget.svg.select('path.line.'+colorid);
+        if (path.empty()){
+            path = widget.svg.append('path');
+            path.attr('class', 'line '+colorid);
+            
+            path.style('stroke-width','2px')
+                .style('fill','none')
+                .style('stroke',color);
+        }
+
+
+        //Transit to new data
+        var lineFunc = d3.line()
+                .x(function(d) { return widget.x_new(d.time); })
+                .y(function(d) { return widget.y(d.val); })
+                .curve(d3.curveStepBefore);
+        var zeroFunc = d3.line()
+                .x(function(d) { return widget.x_new(d.time); })
+                .y(function(d) { return widget.y(0); });
+
+        path.transition()
+            .duration(500)
+            .attr('d', lineFunc(data));
+    },
+
 
     playTime: function(play_stop, speed, ref){
     	var widget = this;
@@ -2232,8 +2401,8 @@ Timeseries.prototype={
                 var newbsel = [bsel[1], 2 * bsel[1] - bsel[0]];
                 widget.brushtime = newbsel.map(widget.x_new.invert);
                 widget.brush.move(widget.gbrush, newbsel);
-                // widget.update();
-                // widget.updateCallback(widget._encodeArgs());
+                widget.update();
+                widget.updateCallback(widget._encodeArgs());
             }, (1000 - speed));
 
     	}
