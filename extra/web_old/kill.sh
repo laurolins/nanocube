fad87b3ed2dbde77d45b3b953ed0566612d0740e
Horace Ip <salivian@gmail.com>
2017-10-13 14:39:40 -0400

    Removed Old WebGUI

--- extra/web_old/kill.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-lsof -w -n -i tcp:29512 | tail -n 1 | cut -d\  -f 2 | xargs kill
-lsof -w -n -i tcp:29513 | tail -n 1 | cut -d\  -f 2 | xargs kill
-lsof -w -n -i tcp:29514 | tail -n 1 | cut -d\  -f 2 | xargs kill
