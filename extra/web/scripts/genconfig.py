f4cf164d9796786e1658ecc9c63379d90ad51d30
Horace Ip <salivian@gmail.com>
2017-10-19 14:59:26 -0400

    genconfig.py now work as the old ncwebviewer-config

--- extra/web/scripts/genconfig.py
+++ extra/web/scripts/genconfig.py
@@ -1,12 +1,85 @@
 from __future__ import print_function, division
 
-import json,sys,argparse,requests
+import os,json,sys,argparse,socket
+import tempfile,shutil,math,random,http.server
+import requests
 
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
+### bbox
+## Find the bounding box
+def findext(server,dim,tiles,maxlevel,func,initp,numoftiles=5):
+    query_url='%s/count.a("%s",dive(tile2d({x},{y},{z}),{drill}),"img")'%(server,dim)
+    
+    p = initp
+    newtiles=[]
+    for location in tiles:
+        res =  requests.get(query_url.format(**location),
+                            verify=False).json()
+        #res_str = res_str.replace('\x00','}') # this is bad
+        #res = json.loads(res_str)
 
+        res = res['root']['children']
+        offset = {'x':location['x']*2**location['drill'],
+                  'y':location['y']*2**location['drill'],
+                  'z':location['z']+location['drill']}
+
+        if 'path' in res[0]:  #if the result is path and not x y ... (lauro...)
+            res =  [ {'x':r['path'][0], 'y':r['path'][1], 'val':r['val']} 
+                     for r in res]
+  
+        #add offset
+        res = [{'x':d['x']+offset['x'],
+                'y':d['y']+offset['y'],
+                'z':offset['z']} for d in res]
+        
+        #find extrememum
+        p={'x':func(p['x'],func(res,key=lambda d: d['x'])['x']),
+           'y':func(p['y'],func(res,key=lambda d: d['y'])['y']),
+           'z':offset['z']}
+
+        newtiles += [ t for t in res \
+                      if  (t['x'] == p['x'] or t['y'] == p['y'])]
+
+    if p['z'] >= maxlevel: # base case
+        return p
+
+    #filter out excessive tiles
+    newtiles = [ t for t in newtiles \
+                 if  (t['x'] == p['x'] or t['y'] == p['y'])]
+
+    random.shuffle(newtiles)
+
+    #Go into the next level
+    nextdrill = min(8,maxlevel-p['z'])
+    for t in newtiles: t['drill'] = nextdrill
+
+    return findext(server,dim,newtiles[:numoftiles],maxlevel,
+                   func,initp,numoftiles)
+
+def num2deg(p):
+    xtile = p['x']
+    ytile = p['y']
+    zoom = p['z']
+    
+    n = 2.0 ** zoom
+    ytile = n-ytile-1
+
+    lon_deg = xtile / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lat_deg = math.degrees(lat_rad)
+    return [lat_deg, lon_deg]
+
+def findbox(server,dim,maxlevel):
+    # min query
+    minp = findext(server,dim,[{'x':0,'y':0,'z':0,'drill':8}],maxlevel,min,
+                   {'x':float('inf'),'y':float('inf')})
+    # max query
+    maxp = findext(server,dim,[{'x':0,'y':0,'z':0,'drill':8}],maxlevel,max,
+                   {'x':float('-inf'),'y':float('-inf')})
+    
+    return (num2deg(minp),num2deg(maxp))
+
+
+### configs
 def initConfig(name,url):
     config = {}
     config['nanocube'] = {name: {'url':url}}
@@ -21,6 +94,12 @@ def main():
     argparser.add_argument('--width',type=int,default=1920)
     argparser.add_argument('--height',type=int,default=1080)
     argparser.add_argument('-s','--server',type=str)
+    argparser.add_argument('-p','--port',type=int,required=False,
+                           const=8000,nargs='?')
+    argparser.add_argument('-w','--web',type=str,required=False,
+                           default=os.environ['NANOCUBE_SRC']+'/extra/web',
+                           nargs='?')
+
     args = argparser.parse_args()
     if args.server is None:
         sys.stderr.writelines(['Please specify a nanocube server with -s\n'])
@@ -30,13 +109,8 @@ def main():
     #download the schema
     url = args.server
     try:
-        #ctx = ssl.create_default_context()
-        #ctx.check_hostname = False
-        #ctx.verify_mode = ssl.CERT_NONE
-        #response = urlopen(url+'/schema',context=ctx)
-        #schema = json.loads(response.read())
         r = requests.get(url+'/schema', allow_redirects=False,verify=False)
-        schema = r.json() #json.loads(r.read())
+        schema = r.json()
     except:
         print ('Fail to read schema from %s'%(url),file=sys.stderr)
         sys.exit(1)
@@ -65,14 +139,34 @@ def main():
         config['widget'][c] = cats[c]
 
     for s in sp:
+        sp[s]['viewbox'] = findbox(url,s,sp[s]['levels']-8)
         config['widget'][s] = sp[s]
 
     for t in ts:
         config['widget'][t] = ts[t]
 
-    print(json.dumps(config,indent=2))
+    if args.port is None:
+        print(json.dumps(config,indent=2))
+    else:        
+        #copy the webgui to a tmp dir start a webserver
+        tmpdir = tempfile.mkdtemp()
+        shutil.copytree(args.web,
+                        tmpdir+'/web')
+        os.chdir(tmpdir+'/web')
+        f = open('config.json','w')
+        f.write(json.dumps(config))
+        f.close()
+        
+        #start httpserver
+        handler= http.server.SimpleHTTPRequestHandler
+        httpd = http.server.HTTPServer(('', args.port),handler);
 
-
+        print("See Nanocubes at http://%s:%d/"%(socket.getfqdn(),
+                                                args.port))
+        httpd.serve_forever()
+        httpd.socket.close()
+        shutil.rmtree(tmpdir)
+        
 def spatialWidget(v,w=1.0):
     levels = v['type'].replace('nc_dim_quadtree_','')
     levels = int(levels)
@@ -80,7 +174,7 @@ def spatialWidget(v,w=1.0):
         'type':'spatial',
         'title': v['name'],
         'tilesurl':'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
-        'coarse_offset':1,
+        'coarse_offset':2,
         'viewbox':[[-85,-180],[85,180]],
         'levels':levels,
         'css':{
