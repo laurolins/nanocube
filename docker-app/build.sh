c77d259612a68fb42c7a2c1223282adeed2bc382
Horace <salivian@gmail.com>
2020-09-17 21:31:58 -0400

    complete docker example with commented out db feature

--- docker-app/build.sh
+++ docker-app/build.sh
@@ -24,13 +24,16 @@ cd /nanocube-src
 bash -c '/nanocube/bin/nanocube create <(gunzip -c ./data/crime50k.csv.gz) ./data/crime50k.map /data/crime50k.nanocube -header' 
 
 #sample database
-apt-get install --no-install-recommends --no-install-suggests -y \
-        python3-pip python3 
-pip3 install --no-cache-dir fastapi uvicorn pandas sqlalchemy
-python3 -c 'import pandas as pd;from sqlalchemy import create_engine; pd.read_csv("data/crime50k.csv.gz").to_sql("crimes",create_engine("sqlite:////data/crimes50k.db"))'
+#apt-get install --no-install-recommends --no-install-suggests -y \
+#        python3-pip python3 
+#pip3 install --no-cache-dir fastapi uvicorn gunicorn \
+#     python-multipart pandas sqlalchemy
+
+#python3 -c 'import pandas as pd;from sqlalchemy import create_engine; d=pd.read_csv("data/crime50k.csv.gz");d=d.rename(columns={"Date":"time","Primary Type":"type"}); d.time=pd.to_datetime(d.time); d.to_sql("crime",create_engine("sqlite:////data/crime50k.db"),index=False)'
+
 
 #cleanup
-apt-get remove --purge -y build-essential git python3-pip
+apt-get remove --purge -y build-essential git
 apt-get autoremove -y && apt-get clean
 rm -rf /var/lib/apt/lists/*
 rm -rf /nanocube-src 
--- docker-app/run.sh
+++ docker-app/run.sh
@@ -5,6 +5,11 @@ export NCDIR=$2
 export WEBCONFIG=$3
 export DBFILE=$4
 
+echo NAME=$NAME
+echo NCDIR=$NCDIR
+echo WEBCONFIG=$WEBCONFIG
+echo DBFILE=$DBFILE
+
 #link custom web config to /www
 ln -s $WEBCONFIG  /www/config.json
 
@@ -12,7 +17,9 @@ ln -s $WEBCONFIG  /www/config.json
 nginx -c /app/nginx.conf
 
 #start db rest server
-uvicorn /nanocube/bin/db_rest_server:app --port 54322 &
+cd /nanocube/bin
+gunicorn -D -w 4 -k uvicorn.workers.UvicornWorker -b 127.0.0.1:54322 \
+         db_rest_server:app
 
 #start nanocubes server with all the nanocube files
 /nanocube/bin/nanocube serve 54321 $NAME=`ls -1 $NCDIR/*.nanocube|paste -sd ':'`
